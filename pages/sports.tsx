64656abdada80f0f44f643785383073e65b9fddc
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-11-13 14:14:09 +0200

    enhance label

--- pages/sports.tsx
+++ pages/sports.tsx
@@ -2,21 +2,21 @@ import { Button, Tooltip, Select } from "@marigold/components";
 import { Info } from '@marigold/icons';
 
 const Label = (
-    <Tooltip.Trigger>
-        <Button>
-            <Info className="text-text-info" />
-        </Button>
-        <Tooltip>Tooltip</Tooltip>
-    </Tooltip.Trigger>
+    <>
+        chose ur fav sport
+        <Tooltip.Trigger>
+            <Button>
+                <Info className="text-text-info" />
+            </Button>
+            <Tooltip>Tooltip</Tooltip>
+        </Tooltip.Trigger>
+    </>
 )
 
 export default function Sports() {
     return (
         <div>
-            <Select label={<>
-                chose ur fav sport
-                {Label}
-            </>}>
+            <Select label={Label}>
                 <Select.Option key="football">football</Select.Option>
                 <Select.Option key="basketball">basketball</Select.Option>
             </Select>
